874d8abd23f3edf5bcbf4808b00fd2698ee4d44e
Dhansukh <81100455+Dhansukh1206@users.noreply.github.com>
2024-08-05 21:32:27 +0530

    bug fix

--- src/components/Calls.js
+++ src/components/Calls.js
@@ -37,109 +37,44 @@ const CallComponent = () => {
     fetchUsers();
   }, []);
 
-  const handleSignalingData = useCallback(
-    (message) => {
-      const data = JSON.parse(message.data);
-
-      switch (data.type) {
-        case "incoming_call":
-          const incomingUser = users.find((user) => user._id === data.from);
-          if (incomingUser) {
-            setSelectedUser(incomingUser);
-            handleOffer(data.offer, incomingUser._id);
-          } else {
-            console.error("User not found for incoming call");
-          }
-          break;
-        case "call_accepted":
-          handleAnswer(data.answer);
-          break;
-        case "candidate":
-          handleCandidate(data.candidate);
-          break;
-        case "end":
-          endCall();
-          break;
-        default:
-          break;
-      }
-    },
-    [users]
-  );
-
-  useEffect(() => {
-    const userId = localStorage.getItem("userId");
-    // const wsUrl = `ws://localhost:8080/?user-id=${userId}`;     // for local test
-    const wsUrl = `wss://desolate-eyrie-13966-6cda0935eea4.herokuapp.com/?user-id=${userId}`; // for live testing
-    ws.current = new WebSocket(wsUrl);
-
-    ws.current.onmessage = handleSignalingData;
-
-    return () => {
-      ws.current.close();
-    };
-  }, [handleSignalingData]);
-
-  const startCall = async (user, video = true) => {
-    if (!user) {
-      setError("Please select a user to call.");
-      return;
+  // Define the endCall function
+  const endCall = useCallback(() => {
+    if (localPeerConnection.current) {
+      localPeerConnection.current.close();
+      localPeerConnection.current = null;
     }
 
-    setSelectedUser(user);
+    if (localVideoRef.current && localVideoRef.current.srcObject) {
+      const localStream = localVideoRef.current.srcObject;
+      localStream.getTracks().forEach((track) => track.stop());
+      localVideoRef.current.srcObject = null;
+    }
 
-    try {
-      const localStream = await navigator.mediaDevices.getUserMedia({
-        video: video,
-        audio: true,
-      });
-      localVideoRef.current.srcObject = localStream;
+    if (remoteStream.current) {
+      remoteStream.current.getTracks().forEach((track) => track.stop());
+      remoteStream.current = new MediaStream();
+    }
 
-      localPeerConnection.current = new RTCPeerConnection();
+    if (remoteVideoRef.current) {
+      remoteVideoRef.current.srcObject = null;
+    }
 
-      localStream.getTracks().forEach((track) => {
-        localPeerConnection.current.addTrack(track, localStream);
-      });
+    setIsCallActive(false);
+    setSelectedUser(null);
 
-      localPeerConnection.current.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-          remoteStream.current.addTrack(track);
-        });
-        remoteVideoRef.current.srcObject = remoteStream.current;
-      };
-
-      localPeerConnection.current.onicecandidate = (event) => {
-        if (event.candidate) {
-          ws.current.send(
-            JSON.stringify({
-              type: "candidate",
-              candidate: event.candidate,
-              target: user._id,
-            })
-          );
-        }
-      };
-
-      const offer = await localPeerConnection.current.createOffer();
-      await localPeerConnection.current.setLocalDescription(offer);
-
+    if (ws.current) {
       ws.current.send(
         JSON.stringify({
-          type: "call",
-          offer: offer,
-          target: user._id,
+          type: "end",
+          target: selectedUser ? selectedUser._id : null,
           userId: localStorage.getItem("userId"),
-          callType: video ? "video" : "voice",
         })
       );
-
-      setIsCallActive(true);
-    } catch (err) {
-      setError("Could not start the call. Please try again.");
     }
-  };
+  }, [selectedUser]);
 
-  const handleOffer = async (offer, fromUserId) => {
+  // Define the handleOffer function
+  const handleOffer = useCallback(async (offer, fromUserId) => {
     try {
       if (!offer || !offer.type || !offer.sdp) {
         throw new Error("Invalid offer received");
@@ -207,9 +142,10 @@ const CallComponent = () => {
     } catch (err) {
       setError("Could not handle the offer. Please try again.");
     }
-  };
+  }, []);
 
-  const handleAnswer = async (answer) => {
+  // Define the handleAnswer function
+  const handleAnswer = useCallback(async (answer) => {
     try {
       await localPeerConnection.current.setRemoteDescription(
         new RTCSessionDescription(answer)
@@ -217,9 +153,10 @@ const CallComponent = () => {
     } catch (err) {
       setError("Could not handle the answer. Please try again.");
     }
-  };
+  }, []);
 
-  const handleCandidate = async (candidate) => {
+  // Define the handleCandidate function
+  const handleCandidate = useCallback(async (candidate) => {
     try {
       if (localPeerConnection.current) {
         await localPeerConnection.current.addIceCandidate(
@@ -231,40 +168,111 @@ const CallComponent = () => {
     } catch (err) {
       setError("Could not add ICE candidate.");
     }
-  };
+  }, []);
 
-  const endCall = () => {
-    if (localPeerConnection.current) {
-      localPeerConnection.current.close();
-      localPeerConnection.current = null;
+  // Define the handleSignalingData function
+  const handleSignalingData = useCallback(
+    (message) => {
+      const data = JSON.parse(message.data);
+
+      switch (data.type) {
+        case "incoming_call":
+          const incomingUser = users.find((user) => user._id === data.from);
+          if (incomingUser) {
+            setSelectedUser(incomingUser);
+            handleOffer(data.offer, incomingUser._id);
+          } else {
+            console.error("User not found for incoming call");
+          }
+          break;
+        case "call_accepted":
+          handleAnswer(data.answer);
+          break;
+        case "candidate":
+          handleCandidate(data.candidate);
+          break;
+        case "end":
+          endCall();
+          break;
+        default:
+          break;
+      }
+    },
+    [users, endCall, handleOffer, handleAnswer, handleCandidate]
+  );
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    // const wsUrl = `ws://localhost:8080/?user-id=${userId}`;     // for local test
+    const wsUrl = `wss://desolate-eyrie-13966-6cda0935eea4.herokuapp.com/?user-id=${userId}`; // for live testing
+    ws.current = new WebSocket(wsUrl);
+
+    ws.current.onmessage = handleSignalingData;
+
+    return () => {
+      ws.current.close();
+    };
+  }, [handleSignalingData]);
+
+  // Define the startCall function
+  const startCall = useCallback(async (user, video = true) => {
+    if (!user) {
+      setError("Please select a user to call.");
+      return;
     }
 
-    if (localVideoRef.current && localVideoRef.current.srcObject) {
-      const localStream = localVideoRef.current.srcObject;
-      localStream.getTracks().forEach((track) => track.stop());
-      localVideoRef.current.srcObject = null;
+    setSelectedUser(user);
+
+    try {
+      const localStream = await navigator.mediaDevices.getUserMedia({
+        video: video,
+        audio: true,
+      });
+      localVideoRef.current.srcObject = localStream;
+
+      localPeerConnection.current = new RTCPeerConnection();
+
+      localStream.getTracks().forEach((track) => {
+        localPeerConnection.current.addTrack(track, localStream);
+      });
+
+      localPeerConnection.current.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteStream.current.addTrack(track);
+        });
+        remoteVideoRef.current.srcObject = remoteStream.current;
+      };
+
+      localPeerConnection.current.onicecandidate = (event) => {
+        if (event.candidate) {
+          ws.current.send(
+            JSON.stringify({
+              type: "candidate",
+              candidate: event.candidate,
+              target: user._id,
+            })
+          );
+        }
+      };
+
+      const offer = await localPeerConnection.current.createOffer();
+      await localPeerConnection.current.setLocalDescription(offer);
+
+      ws.current.send(
+        JSON.stringify({
+          type: "call",
+          offer: offer,
+          target: user._id,
+          userId: localStorage.getItem("userId"),
+          callType: video ? "video" : "voice",
+        })
+      );
+
+      setIsCallActive(true);
+    } catch (err) {
+      setError("Could not start the call. Please try again.");
     }
-
-    if (remoteStream.current) {
-      remoteStream.current.getTracks().forEach((track) => track.stop());
-      remoteStream.current = new MediaStream();
-    }
-
-    if (remoteVideoRef.current) {
-      remoteVideoRef.current.srcObject = null;
-    }
-
-    setIsCallActive(false);
-    setSelectedUser(null);
-
-    ws.current.send(
-      JSON.stringify({
-        type: "end",
-        target: selectedUser ? selectedUser._id : null,
-        userId: localStorage.getItem("userId"),
-      })
-    );
-  };
+  }, []);
 
   const handleUserSelection = (user) => {
     setSelectedUser(user);
