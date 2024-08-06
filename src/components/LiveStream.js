42fd4261de38085f0c5b8a914c4c6e5cd583546a
Dhansukh <81100455+Dhansukh1206@users.noreply.github.com>
2024-08-07 01:21:13 +0530

    try to make fixing live stream bug

--- src/components/LiveStream.js
+++ src/components/LiveStream.js
@@ -12,6 +12,10 @@ const LiveStream = () => {
 
   const initializeWebSocket = useCallback(() => {
     const userId = localStorage.getItem("userId");
+    // const wsUrl = `ws://localhost:8080/?stream=${userId}`; // for local test
+    const wsUrl = `wss://desolate-eyrie-13966-6cda0935eea4.herokuapp.com/?stream=${userId}`; // for live testing
+    ws.current = new WebSocket(wsUrl);
+
     wsRef.current = new WebSocket(`ws://localhost:8080/?stream-id=${userId}`);
 
     wsRef.current.onopen = () => {
